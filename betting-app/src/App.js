192e03f9dadc17a0c774654df6a99d1d0fc9589e
ching <ching2020huang@gmail.com>
2024-04-27 11:59:12 -0400

    Add routers

--- betting-app/src/App.js
+++ betting-app/src/App.js
@@ -1,169 +1,38 @@
-import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+// App.js
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Link,  Routes } from 'react-router-dom';
+import Home from './Home';
+import Profile from './Profile';
+import CustomLink from './Components/CustomLink'
 function App() {
-  const [graphData, setGraphData] = useState({
-    standard: [],
-    volatile: [],
-    biased_positive: [],
-    biased_negative: []
-  });
 
-  const [graphType, setGraphType] = useState('all');
-  const prevType = useRef(null);
-
-  useEffect(() => {
-    const fetchData = (type) => {
-      axios.get(`http://localhost:5000/random-number/${type}`)
-        .then(response => {
-          const newNumber = response.data.random_number;
-          updateChartData(newNumber, type);
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
-    };
-
-    if (graphType === 'all') {
-      ['standard', 'volatile', 'biased_positive', 'biased_negative'].forEach(type => fetchData(type));
-    } else {
-      fetchData(graphType);
-    }
-    const intervalId = setInterval(() => {
-      if (graphType === 'all') {
-        ['standard', 'volatile', 'biased_positive', 'biased_negative'].forEach(type => fetchData(type));
-      } else {
-        fetchData(graphType);
-      }
-    }, 2000);
-    return () => clearInterval(intervalId);
-  }, [graphType]);
-
-  const updateChartData = (newNumber, type) => {
-    setGraphData(prevData => {
-      const maxDataPoints = 15;
-      const newData = { ...prevData };
-      const labels = newData[type].labels || [];
-      const data = newData[type].data || [];
-      const time = new Date().toISOString().split('T')[1].split(':')
-      labels.push(time[1]+":"+time[2].split(".")[0]);
-      data.push(newNumber);
-
-      if (labels.length > maxDataPoints) {
-        labels.shift();
-        data.shift();
-      }
-
-      newData[type] = { labels, data };
-      return newData;
-    });
+ const linkStyle = {
+    color: '#007bff', /* Link color */
+    textDecoration: 'none', /* Remove underline */
+    fontWeight: 'bold', /* Bold text */
+    transition: 'color 0.3s' /* Smooth color transition */
   };
 
-  const bigComponents = Object.keys(graphData).map((type) => (
-    <div key={type} style={{ width: '100%', height: '50%' }}>
-      <h3>{type.replace('_', ' ').toUpperCase()}</h3>
-      <Line
-        data={{
-          labels: graphData[type].labels,
-          datasets: [{
-            label: `Random Numbers - ${type}`,
-            data: graphData[type].data,
-            borderColor: 'rgb(75, 192, 192)',
-            backgroundColor: 'rgba(75, 192, 192, 0.5)',
-          }]
-        }}
-        options={{
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            tooltip: {
-              mode: 'index',
-              intersect: false,
-            },
-          },
-          scales: {
-            x: {
-              display: true,
-            },
-            y: {
-              type: 'linear',
-              display: true,
-              position: 'left',
-            },
-          }
-        }}
-      />
-    </div>
-  ));
-
-  const graphComponents = Object.keys(graphData).map((type) => (
-    <div key={type} style={{ width: '50%', height: '45%', marginBottom: '50px' }}>
-      <h3>{type.replace('_', ' ').toUpperCase()}</h3>
-      <Line
-        data={{
-          labels: graphData[type].labels,
-          datasets: [{
-            label: `Random Numbers - ${type}`,
-            data: graphData[type].data,
-            borderColor: 'rgb(75, 192, 192)',
-            backgroundColor: 'rgba(75, 192, 192, 0.5)',
-          }]
-        }}
-        options={{
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            tooltip: {
-              mode: 'index',
-              intersect: false,
-            },
-          },
-          scales: {
-            x: {
-              display: true,
-            },
-            y: {
-              type: 'linear',
-              display: true,
-              position: 'left',
-            },
-          }
-        }}
-      />
-    </div>
-  ));
+  const hoverStyle = {
+    color: '#0056b3' /* Change color on hover */
+  };
 
   return (
-    <div style={{ display: 'flex', width: '100%', height: '90vh' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', margin: '10px' }}>
-        <button style={{ margin: '5px', width: '150px', height: '50px', border: 'none', backgroundColor: 'teal', color: 'white', fontSize: '20px' }} onClick={() => setGraphType('standard')}>Standard</button>
-        <button style={{ margin: '5px', width: '150px', height: '50px', border: 'none', backgroundColor: 'teal', color: 'white', fontSize: '20px' }} onClick={() => setGraphType('volatile')}>Volatile</button>
-        <button style={{ margin: '5px', width: '150px', height: '50px', border: 'none', backgroundColor: 'teal', color: 'white', fontSize: '20px' }} onClick={() => setGraphType('biased_positive')}>Biased Pos</button>
-        <button style={{ margin: '5px', width: '150px', height: '50px', border: 'none', backgroundColor: 'teal', color: 'white', fontSize: '20px' }} onClick={() => setGraphType('biased_negative')}>Biased Neg</button>
-        <button style={{ margin: '5px', width: '150px', height: '200px', border: 'none', backgroundColor: 'teal', color: 'white', fontSize: '25px' }} onClick={() => setGraphType('all')}>All Graphs</button>
+    <Router>
+      <div style={{display: 'flex'}}>
+        <div style={{ minHeight: '100vh' }}>
+            <nav style={{ width: '200px',  position: 'absolute', top: 80, right: 0, margin: '20px' }}>
+              <CustomLink to="/" >Home</CustomLink>
+              <CustomLink to="/profile">Profile</CustomLink>
+            </nav>
+        </div>
+         <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/profile" element={<Profile/>} />
+        </Routes>
       </div>
-      <div style={{ display: 'flex', flexGrow: 1, flexWrap: 'wrap' }}>
-        {graphType === 'all' ? graphComponents : bigComponents.filter(comp => comp.key === graphType)}
-      </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ betting-app/src/Profile.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect, useRef } from 'react';
+import PageHeader from './Components/PageHeader';
+import TransactionView from './Components/TransactionView';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import ConfirmButton from './Components/ConfirmButton';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+function Profile() {
+
+  const profit = '+0';
+
+  const transactions = [
+    { id: 1, date: '2022-01-01', type: 'avg delay', amount: 50, profit: -10 },
+    { id: 2, date: '2022-01-01', type: 'total delay', amount: 50, profit: 20 },
+    { id: 3, date: '2022-01-01', type: 'number of delays', amount: 50, profit: 100  },
+    // Add more transactions here
+  ];
+
+  let lastTenTransactions = [];
+  const transactionType = 'all';
+    if (transactionType == 'all') {
+      lastTenTransactions = transactions.slice(-10);
+    } else {
+      for (let t in transactions) {
+      console.log(transactions[t]);
+          if (transactions[t].description == 'Coffee') {
+              lastTenTransactions.push(transactions[t]);
+          }
+      }
+    }
+
+   const chartRef = useRef(null);
+    const transactionViewRef = useRef(null);
+  const profitData = [{date:'2024-04-01', totalProfit:'10'},{date:'2024-04-02', totalProfit:'20'}]
+    useEffect(() => {
+      const ctx = chartRef.current.getContext('2d');
+
+      ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Legend
+      );
+      if (chartRef.current && transactionViewRef.current) {
+            const transactionViewHeight = transactionViewRef.current.clientHeight;
+            chartRef.current.height = transactionViewHeight * 0.8;
+      }
+      const chart = new ChartJS(ctx, {
+        type: 'line',
+        data: {
+          labels: profitData.map(item => item.date),
+          datasets: [{
+            label: 'Total Profit',
+            data: profitData.map(item => item.totalProfit),
+            borderColor: 'blue',
+            borderWidth: 2,
+            fill: false,
+          }]
+        },
+        options: {
+          scales: {
+            x: {
+              type: 'category',
+              title: {
+                display: true,
+                text: 'Date'
+              }
+            },
+            y: {
+              title: {
+                display: true,
+                text: 'Average Profit'
+              }
+            }
+          }
+        }
+      });
+
+      return () => {
+        chart.destroy();
+      };
+    }, [profitData]);
+
+  return (
+ <div style={{ display: 'flex', flexDirection: 'column'}}>
+   <PageHeader>Profile</PageHeader>
+   <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+        <div style= {{margin: '50px'}}>
+        <p style={{ textAlign: 'left' }}>You are currently {profit}</p>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', margin: '50px',  width: '100%'}}>
+          <div style={{ flex: 1}}>
+            <h2>Total Daily Profits </h2>
+            <canvas ref={chartRef} style={{ height:' 100%', width: '100%', padding:'10px' }} ></canvas>
+          </div>
+           <div ref={transactionViewRef} style={{ flex: 1}}>
+            <TransactionView transactions={lastTenTransactions}/>
+           </div>
+        </div>
+   </div>
+
+
+
+ </div>
+
+  );
+}
+
+export default Profile;
