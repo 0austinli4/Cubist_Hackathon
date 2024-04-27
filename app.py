6f3f90beab7a8ece4c949afcc697e096e3737bd0
John Matters <john.matters@princeton.edu>
2024-04-27 15:32:55 -0400

    added final

--- app.py
+++ app.py
@@ -19,23 +19,51 @@ current_numbers = {
     'biased_negative': 0
 }
 
+standard = []
+delay = []
+iStandard = 0
+iDelay = 0
+
+def getData(file_path, array):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    for index, line in enumerate(lines):
+        number = float(line.strip())
+        array.append(number)
+    return array
+
 @app.route('/')
 def index():
     return "Real-Time Betting App"
 
 def random_walk_adjustment(walk_type):
-    if walk_type == 'standard':
-        return random.random()-0.5
-    elif walk_type == 'volatile':
-        return random.randint(-5, 5)
-    elif walk_type == 'biased_positive':
+    # if walk_type == 'standard':
+    #     return random.random()-0.5
+    # elif walk_type == 'volatile':
+    #     return random.randint(-5, 5)
+    if walk_type == 'biased_positive':
         return random.randint(0, 3)
     elif walk_type == 'biased_negative':
         return random.randint(-3, 0)
 
 @app.route('/random-number/<walk_type>')
 def random_number(walk_type):
-    if walk_type in current_numbers:
+    if walk_type in ["standard"]:
+        global iStandard
+        iStandard = iStandard % len(standard)
+        current_numbers['standard'] = standard[iStandard]
+        iStandard += 1
+
+        return jsonify({'random_number': current_numbers[walk_type], 'bid': (current_numbers[walk_type]-1-random.random()), 'ask':(current_numbers[walk_type]+1+random.random())})
+    elif walk_type in ["volatile"]:
+        global iDelay
+        iDelay = iDelay % len(delay)
+        current_numbers['volatile'] = delay[iDelay]
+        iDelay += 1
+        
+        return jsonify({'random_number': current_numbers[walk_type], 'bid': (current_numbers[walk_type]-1-random.random()), 'ask':(current_numbers[walk_type]+1+random.random())})
+
+    elif walk_type in current_numbers:
         current_numbers[walk_type] += random_walk_adjustment(walk_type)
         return jsonify({'random_number': current_numbers[walk_type], 'bid': (current_numbers[walk_type]-1-random.random()), 'ask':(current_numbers[walk_type]+1+random.random())})
     else:
@@ -62,4 +90,7 @@ def no_orders(walk_type):
     return jsonify({'orders':[{'orderID':0, 'type': 0, 'price':0, 'amount':0, 'executed':0}]})
 
 if __name__ == '__main__':
+    index = 0
+    getData('average_time_data', standard)
+    getData('delay_time', delay)
     socketio.run(app)
